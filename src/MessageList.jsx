071dee1bac27ae40d280cbe523a29bfa23973bb0
Your Name <your@email.com>
2019-05-07 16:22:25 +0000

    Implement components

---
+++ src/MessageList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class MassageList extends React.Component {
+  render() {
+    return (
+      <div className="message system">
+        Anonymous1 changed their name to nomnom.
+      </div>
+    );
+  }
+}
+export default MassageList;
